bdd3e9adac509ea82cd9380ad66af84e60bad09c
Jericho Hasselbush <jericho@sal-et-lucem.com>
2023-11-25 17:34:57 -0500

    PC2 table generated.

--- Sources/des/des.swift
+++ Sources/des/des.swift
@@ -164,6 +164,19 @@ extension DES {
                               21, 13, 5, 28, 20, 12, 4]
         return BITable(values: table)}()
 
+    static let pc2: BITable = {
+        let table: [UInt8] = [
+            14, 17, 11, 24, 1, 5,
+            3, 28, 15, 6, 21, 10,
+            23, 19, 12, 4, 26, 8,
+            16, 7, 27, 20, 13, 2,
+            41, 52, 31, 37, 47, 55,
+            30, 40, 51, 45, 33, 48,
+            44, 49, 39, 56, 34, 53,
+            46, 42, 50, 36, 29, 32]
+        return BITable(values: table)
+    }()
+
     static let s1: BITable = {
         let table: [UInt8] = [
             14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,

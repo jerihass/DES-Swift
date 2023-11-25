1956bcf2cd98f8347dfa2242e98e0df3d7894c06
Jericho Hasselbush <jericho@sal-et-lucem.com>
2023-11-24 21:34:24 -0500

    Tables and extensions to UInt32 and 16...

--- Sources/des/des.swift
+++ Sources/des/des.swift
@@ -18,6 +18,7 @@ extension BinaryInteger {
     ///   - lhs: Binary Integer to shift
     ///   - rhs: places to shift
     /// - Returns: Left circular shifts: 0b1011 -> 0b0111
+    mutating func lcs(by: Self) -> Self { self <<< by }
     static func <<< <RHS>(lhs: inout Self, rhs: RHS) -> Self where RHS: BinaryInteger {
          let shiftCount = Int(rhs) % lhs.bitWidth
          if shiftCount > 0 {
@@ -32,6 +33,8 @@ extension BinaryInteger {
     ///   - lhs: Binary Integer to shift
     ///   - rhs: places to shift
     /// - Returns: Right circular shifts: 0b1011 -> 0b1101
+    ///
+    mutating func rcs(by: Self) -> Self { self >>> by }
     static func >>> <RHS>(lhs: inout Self, rhs: RHS) -> Self where RHS: BinaryInteger {
         let shiftCount = Int(rhs) % lhs.bitWidth
         if shiftCount > 0 {
@@ -40,4 +43,98 @@ extension BinaryInteger {
         }
         return lhs
     }
+}
+
+extension UInt64 {
+    func split() -> (UInt32, UInt32) {
+        let fraction = self.bitWidth / 2
+        let left = UInt32(self &>> fraction)
+        let right = UInt32(((self &<< fraction) >> fraction))
+        return (left, right)
+    }
+}
+
+extension UInt32 {
+    func split() -> (UInt16, UInt16) {
+        let fraction = self.bitWidth / 2
+        let left = UInt16(self &>> fraction)
+        let right = UInt16(((self &<< fraction) >> fraction))
+        return (left, right)
+    }
+}
+extension UInt16 {
+    func split() -> (UInt8, UInt8) {
+        let fraction = self.bitWidth / 2
+        let left = UInt8(self &>> fraction)
+        let right = UInt8(((self &<< fraction) >> fraction))
+        return (left, right)
+    }
+}
+
+struct BITable {
+    var values: [any BinaryInteger]
+    func lookup(_ index: Int) -> (any BinaryInteger)? {
+        guard index < values.count else { return nil }
+        return values[index]
+    }
+}
+
+class DES {
+    static let ip: BITable = {
+        let table: [UInt8] = [58, 50, 42, 34, 26, 18, 10, 2,
+                              60, 52, 44, 36, 28, 20, 12, 4,
+                              62, 54, 46, 38, 30, 22, 14, 6,
+                              64, 56, 48, 40, 32, 24, 16, 8,
+                              57, 49, 41, 33, 25, 17, 9, 1,
+                              59, 51, 43, 35, 27, 19, 11 , 3,
+                              61, 53, 45, 37, 29, 21, 13 , 5,
+                              63, 55, 47, 39, 31, 23, 15 , 7]
+        return BITable(values: table)}()
+
+    static let ip_inv: BITable = {
+        let table: [UInt8] = [40, 8, 48, 16, 56, 14, 64, 32,
+                              39, 7, 47, 15, 55, 23, 63, 31,
+                              38, 6, 46, 14, 54, 22, 62, 30,
+                              37, 5, 45, 13, 53, 21, 61, 29,
+                              36, 4, 44, 12, 52, 20, 60, 28,
+                              35, 3, 43, 11, 51, 19, 59, 27,
+                              34, 2, 42, 10, 50, 18, 58, 26,
+                              33, 1, 41, 9, 49, 17, 57, 25]
+        return BITable(values: table)}()
+
+    static let e_bit: BITable = {
+        let table: [UInt8] = [32, 1, 2, 3, 4, 5,
+                              4, 5, 6, 7, 8, 9,
+                              8, 9, 10, 11, 12, 13,
+                              12, 13, 14, 15, 16, 17,
+                              16, 17, 18, 19, 20, 21,
+                              20, 21, 22, 23, 24, 25,
+                              24, 25, 26, 27, 28, 29,
+                              28, 29, 30, 31, 32, 1]
+        return BITable(values: table)}()
+
+    static let p: BITable = {
+        let table: [UInt8] = [16, 7, 20, 21,
+                              29, 12, 28, 17,
+                              1, 15, 23, 26,
+                              5, 18, 31, 10,
+                              2, 8, 24, 14,
+                              32, 27, 3, 9,
+                              19, 13, 30, 6,
+                              22, 11, 4, 25]
+        return BITable(values: table)}()
+
+    static let pc1_left: BITable = {
+        let table: [UInt8] = [57, 49, 41, 33, 25, 17, 9,
+                              1, 58, 50, 42, 34, 26, 18,
+                              10, 2, 59, 51, 43, 35, 27,
+                              19, 11, 3, 60, 52, 44, 36]
+        return BITable(values: table)}()
+
+    static let pc1_right: BITable = {
+        let table: [UInt8] = [63, 55, 47, 39, 31, 23, 15,
+                              7, 62, 54, 46, 38, 30, 22,
+                              14, 6, 61, 53, 45, 37, 29,
+                              21, 13, 5, 28, 20, 12, 4]
+        return BITable(values: table)}()
 }
--- Tests/desTests/desTests.swift
+++ Tests/desTests/desTests.swift
@@ -6,8 +6,25 @@ final class desTests: XCTestCase {
         var bits: UInt8 = 0b10000001
         XCTAssertEqual(bits <<< 1, 0b00000011)
         bits = 0b10000001
-        print(bits)
         XCTAssertEqual(bits >>> 1, 0b11000000)
-        print(bits)
+
+        bits = 0b10000001
+        XCTAssertEqual(bits.lcs(by: 1), 0b00000011)
+        bits = 0b10000001
+        XCTAssertEqual(bits.rcs(by: 1), 0b11000000)
+    }
+
+    func test_shouldSplit64Bit() throws {
+        let bits: UInt64 = 0b1111111111111111111111111111111010000000000000000000000000000001
+        let split = bits.split()
+        XCTAssertEqual(split.0, 0b11111111111111111111111111111110)
+        XCTAssertEqual(split.1, 0b10000000000000000000000000000001)
+    }
+
+    func test_shouldLookupSomething() throws {
+        let table = DES.ip
+        print(table)
+        let value = table.lookup(0) as? UInt8
+        XCTAssertEqual(value, UInt8(58))
     }
 }

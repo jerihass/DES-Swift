36f6f8dd4b97c3d70a27cf9d3d90892d275ccbc2
Jericho Hasselbush <jericho@sal-et-lucem.com>
2023-11-25 17:32:19 -0500

    All S-tables generated.

--- Sources/des/des.swift
+++ Sources/des/des.swift
@@ -50,15 +50,11 @@ class DES {
     }
 
     internal func sfunction(_ bit8: UInt8, sTable: BITable ) -> UInt8? {
-        // determine row
         let r1 = bit8.getBit(3)
         let r2 = bit8.getBit(8)
         let row = (r1 << 1) | r2
-
         let column = (bit8 << 3) >> 5
-
         let index = (row * 16) + column
-
         let value = sTable.lookup(Int(index)) as? UInt8
         return value
     }
@@ -85,6 +81,8 @@ class DES {
         return pc1
     }
 }
+
+protocol STable {}
 
 extension DES {
     static let interationShifts = {
@@ -172,6 +170,73 @@ extension DES {
             0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
             4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
             15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]
+        return BITable(values: table)
+    }()
+
+    static let s2: BITable = {
+        let table: [UInt8] = [
+            15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
+            3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
+            0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15,
+            13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]
+        return BITable(values: table)
+    }()
+
+    static let s3: BITable = {
+        let table: [UInt8] = [
+            10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
+            13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
+            13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7,
+            1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]
+        return BITable(values: table)
+    }()
+
+    static let s4: BITable = {
+        let table: [UInt8] = [
+            7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
+            13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
+            10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
+            3, 1, 5, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]
+        return BITable(values: table)
+    }()
+
+    static let s5: BITable = {
+        let table: [UInt8] = [
+            2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
+            14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6,
+            4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14,
+            11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3
+            ]
+        return BITable(values: table)
+    }()
+
+    static let s6: BITable = {
+        let table: [UInt8] = [
+            12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
+            10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8,
+            9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6,
+            4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13
+            ]
+        return BITable(values: table)
+    }()
+
+    static let s7: BITable = {
+        let table: [UInt8] = [
+            4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
+            13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6,
+            1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2,
+            6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12
+            ]
+        return BITable(values: table)
+    }()
+
+    static let s8: BITable = {
+        let table: [UInt8] = [
+            13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
+            1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
+            7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
+            2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11
+        ]
         return BITable(values: table)
     }()
 }
